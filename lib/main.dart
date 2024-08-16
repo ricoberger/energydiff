7195ae63046f7f64ecb8023672eb18166d740a0f
Rico Berger <ricoberger@users.noreply.github.com>
2024-08-16 11:05:50 +0200

    Add Tooltips (#5)
    Whenever we show the burned, consumed or difference energy, we now also
    show a tooltip. This was done so that we can show the active and basal
    energy for the burned energy.
    
    This also fixes the tooltip for the bar chart, which contained an
    unnecessary new line after we displayed the difference.

--- lib/main.dart
+++ lib/main.dart
@@ -17,32 +17,41 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TooltipVisibility(
-      visible: false,
-      child: MaterialApp(
-        title: 'Energy Diff',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: const ColorScheme(
-            brightness: Brightness.dark,
-            primary: Color(0xff171f2c),
-            onPrimary: Colors.white,
-            secondary: Color(0xff171f2c),
-            onSecondary: Colors.white,
-            error: Colors.red,
-            onError: Colors.white,
-            surface: Color(0xff171f2c),
-            onSurface: Colors.white,
+    return MaterialApp(
+      title: 'Energy Diff',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xff171f2c),
+          onPrimary: Colors.white,
+          secondary: Color(0xff171f2c),
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          surface: Color(0xff171f2c),
+          onSurface: Colors.white,
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff171f2c),
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        tooltipTheme: const TooltipThemeData(
+          padding: EdgeInsets.all(8),
+          preferBelow: false,
+          textStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
           ),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color(0xff171f2c),
-            foregroundColor: Colors.white,
-            elevation: 0,
+          decoration: BoxDecoration(
+            color: Color(0xff171f2c),
+            borderRadius: BorderRadius.all(Radius.circular(4)),
           ),
         ),
-        debugShowCheckedModeBanner: false,
-        home: const Home(),
       ),
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
     );
   }
 }
